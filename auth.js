185623e90fcb619bf254a40207c0c5f758b67359
Gustav Hedin <penthero@gmail.com>
2015-02-05 15:42:54 +0100

    Fixed so changing account does not mess up page

--- auth.js
+++ auth.js
@@ -42,6 +42,7 @@
 			// client flow. The current function is called when that flow completes.
 			$('#login-link').attr('disabled', false);
 			$('#login-link').click(function() {
+				$('#search-container').html("");
 				gapi.auth.authorize({
 					client_id: OAUTH2_CLIENT_ID,
 					scope: OAUTH2_SCOPES,
